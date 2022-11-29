5822de72b5fa35325ce9ca815ce68fc6143257fc
Chibuike Onyejiaka <chibuikeaugustine147@gmail.com>
2022-11-29 21:05:20 +0000

    digits comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
-uthor - Tolulope Fakunle
-
-for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
+#!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
         else:
-            print("{}{}".format(digit1, digit2), end=", ")
+            print("{:d}{:d}".format(i, j), end=", ")
