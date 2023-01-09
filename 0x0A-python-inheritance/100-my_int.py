8c07b7f901cb0d1544ea9aff3e10d942400d1673
Chibuike Onyejiaka <chibuikeaugustine147@gmail.com>
2023-01-09 13:22:10 +0000

    python inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+s module defines a class MyInt that inherits from int"""
+
+
+class MyInt(int):
+    """Invert int operators == and !="""
+
+    def __eq__(self, value):
+        """Override == opeartor with != behavior"""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior"""
+        return self.real == value
