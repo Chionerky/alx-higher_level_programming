336b22001ffa80f3d1be00afbebdae83c66ffbde
Chibuike Onyejiaka <chibuikeaugustine147@gmail.com>
2023-01-10 15:23:53 +0000

    python input and output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a UTF8 text file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
